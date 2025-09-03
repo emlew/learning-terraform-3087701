79191f2918fb2b49e83827de72365745847fdb9d
Emily Lewis <97252954+emlew@users.noreply.github.com>
2025-09-03 13:20:58 -0400

    Downgrade autoscaling version

--- main.tf
+++ main.tf
@@ -33,9 +33,9 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.1"
+  version = "6.5.2"
   
   name = "blog"
   min_size = 1
